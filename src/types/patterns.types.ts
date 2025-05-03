8b6a851040fa9ded68fda09db90ca0b1b283fec9
Jefn Rowell <j3ff.rowell@gmail.com>
2025-05-03 09:24:46 -0600

    Changing pattern type to RegExp

--- src/types/patterns.types.ts
+++ src/types/patterns.types.ts
@@ -4,6 +4,6 @@ export interface Pattern {
     isValidityCustomizable: boolean;
     hasCustomValidity: boolean;
     validityEndpoints: string[];
-    pattern: string;
+    pattern: RegExp;
     entropy: number;
 }
