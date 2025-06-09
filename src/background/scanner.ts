2b198da63e669cf2a6d2ba8e34c02a9e0161026a
Jefn Rowell <j3ff.rowell@gmail.com>
2025-06-09 08:25:09 -0600

    feat(scanner): added aws session key to the scanner.

--- src/background/scanner.ts
+++ src/background/scanner.ts
@@ -1,12 +1,28 @@
 import { Finding, Occurrence } from '../types/findings.types';
 import { detectAwsAccessKeys } from './detectors/aws/access_keys/access_keys';
+import { detectAwsSessionKeys } from './detectors/aws/session_keys/session_keys';
 
 export async function findSecrets(content: string, url: string): Promise<Finding[]> {
     const findings: Finding[] = [];
-    const awsMatches: Occurrence[] = await detectAwsAccessKeys(content, url);
+    const awsAccessKeyMatches: Occurrence[] = await detectAwsAccessKeys(content, url);
+    const awsSessionKeyMatches: Occurrence[] = await detectAwsSessionKeys(content, url);
 
-    if (awsMatches.length > 0) {
-        awsMatches.forEach(occurrence => {
+    if (awsAccessKeyMatches.length > 0) {
+        awsAccessKeyMatches.forEach(occurrence => {
+            const f: Finding = {
+                numOccurrences: 1,
+                secretType: occurrence.secretType,
+                secretValue: occurrence.secretValue,
+                validity: "valid",
+                validatedAt: new Date().toISOString(),
+                fingerprint: occurrence.fingerprint,
+                occurrences: new Set([occurrence])
+            }
+            findings.push(f);
+        });
+    }
+    if (awsSessionKeyMatches.length > 0) {
+        awsSessionKeyMatches.forEach(occurrence => {
             const f: Finding = {
                 numOccurrences: 1,
                 secretType: occurrence.secretType,
