0582136dcef19fb5595f1e77baf19d6a6e1a9269
Jefn Rowell <j3ff.rowell@gmail.com>
2025-06-11 18:36:04 -0600

    refactor(scanner): adding createFindings to refactor duplicate code.

--- src/background/scanner.ts
+++ src/background/scanner.ts
@@ -4,37 +4,29 @@ import { detectAwsSessionKeys } from './detectors/aws/session_keys/session_keys'
 
 export async function findSecrets(content: string, url: string): Promise<Finding[]> {
     const findings: Finding[] = [];
-    const awsAccessKeyMatches: Occurrence[] = await detectAwsAccessKeys(content, url);
-    const awsSessionKeyMatches: Occurrence[] = await detectAwsSessionKeys(content, url);
 
-    if (awsAccessKeyMatches.length > 0) {
-        awsAccessKeyMatches.forEach(occurrence => {
-            const f: Finding = {
-                numOccurrences: 1,
-                secretType: occurrence.secretType,
-                secretValue: occurrence.secretValue,
-                validity: "valid",
-                validatedAt: new Date().toISOString(),
-                fingerprint: occurrence.fingerprint,
-                occurrences: new Set([occurrence])
-            }
-            findings.push(f);
-        });
-    }
-    if (awsSessionKeyMatches.length > 0) {
-        awsSessionKeyMatches.forEach(occurrence => {
-            const f: Finding = {
-                numOccurrences: 1,
-                secretType: occurrence.secretType,
-                secretValue: occurrence.secretValue,
-                validity: "valid",
-                validatedAt: new Date().toISOString(),
-                fingerprint: occurrence.fingerprint,
-                occurrences: new Set([occurrence])
-            }
-            findings.push(f);
-        });
-    }
+    const awsAccessKeyMatches: Occurrence[] = await detectAwsAccessKeys(content, url);
+    createFindings(findings, awsAccessKeyMatches)
+
+    const awsSessionKeyMatches: Occurrence[] = await detectAwsSessionKeys(content, url);
+    createFindings(findings, awsSessionKeyMatches)
 
     return findings;
+}
+
+async function createFindings(findings: Finding[], matches: Occurrence[]) {
+    if (matches.length > 0) {
+        matches.forEach(occurrence => {
+            const f: Finding = {
+                numOccurrences: 1,
+                secretType: occurrence.secretType,
+                secretValue: occurrence.secretValue,
+                validity: "valid",
+                validatedAt: new Date().toISOString(),
+                fingerprint: occurrence.fingerprint,
+                occurrences: new Set([occurrence])
+            }
+            findings.push(f);
+        });
+    }
 }
