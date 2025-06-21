86b6756bfb73d0afe0b0e59576db78847bac3334
Jefn Rowell <j3ff.rowell@gmail.com>
2025-06-21 08:41:55 -0600

    feat: added anthropic validity helper to be called from the popup and options pages.

---
+++ src/utils/validators/anthropic/anthropicValidityHelper.ts
@@ -0,0 +1,48 @@
+import { Finding } from "src/types/findings.types";
+import { retrieveFindings, storeFindings } from "../../helpers/common";
+import { validateAnthropicCredentials } from "./anthropic";
+
+export async function anthropicValidityHelper(finding: Finding): Promise<void> {
+    for (const anthropicOccurrence of Object.values(finding.secretValue)) {
+        if (!anthropicOccurrence.api_key) {
+            continue;
+        }
+
+        const validationResult = await validateAnthropicCredentials(
+            anthropicOccurrence.api_key
+        );
+
+        if (!validationResult.valid) {
+            retrieveFindings().then((existingFindings) => {
+                const index = existingFindings.findIndex(
+                    (f) => f.fingerprint === finding.fingerprint
+                );
+                existingFindings[index].validity = "invalid";
+                existingFindings[index].validatedAt = new Date().toISOString();
+                storeFindings(existingFindings);
+            });
+            break;
+        } else if (finding.validity === 'invalid') {
+            // this handles situations where the key was deactivated and then re-activated later on
+            retrieveFindings().then((existingFindings) => {
+                const index = existingFindings.findIndex(
+                    (f) => f.fingerprint === finding.fingerprint
+                );
+                existingFindings[index].validity = "valid";
+                existingFindings[index].validatedAt = new Date().toISOString();
+                storeFindings(existingFindings);
+            });
+            break;
+        } else {
+            // is still valid, update the timestamp
+            retrieveFindings().then((existingFindings) => {
+                const index = existingFindings.findIndex(
+                    (f) => f.fingerprint === finding.fingerprint
+                );
+                existingFindings[index].validity = "valid";
+                existingFindings[index].validatedAt = new Date().toISOString();
+                storeFindings(existingFindings);
+            });
+        }
+    }
+}
